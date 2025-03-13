d53929e56e00091c476329d1c2c22d5efc594a20
Aarin Bailey <aarin.m.bailey@gmail.com>
2025-03-12 20:19:39 -0500

    Update

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useReducer, useEffect, useState } from "react";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 const AuthContext = createContext();
 
